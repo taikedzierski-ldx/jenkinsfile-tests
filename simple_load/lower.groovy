bd8971665a8275d7187fe78fbcc6cf05f4078d0c
Tai Kedzierski <>
2022-05-09 09:54:29 +0100

    instantiation test

--- simple_load/lower.groovy
+++ simple_load/lower.groovy
@@ -1,3 +1,11 @@
-def say_hi(someone) {
-    println "Hello $someone"
+class Lower {
+
+    def Lower() {}
+
+    def say_hi(someone) {
+        println "Hello $someone"
+    }
+
 }
+
+return new Lower()
