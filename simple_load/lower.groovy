15f2326fab41fc9583e4a42ba4eca138b371259c
Tai Kedzierski <>
2022-05-09 11:11:59 +0100

    Check instantiation behaviour with load

--- simple_load/lower.groovy
+++ simple_load/lower.groovy
@@ -1,5 +1,13 @@
+// Don't use "def" --> makes global variable-like item
+// Otherwise, the variable goes to the main runtime method's scope
+main_greeting = "Hello"
+
 def say_hi(someone) {
-    println "Hello $someone"
+    println "${main_greeting} $someone"
+}
+
+def configure_greeting(new_greeting) {
+    main_greeting = new_greeting
 }
 
 return this
--- simple_load/upper.groovy
+++ simple_load/upper.groovy
@@ -1,3 +1,10 @@
-lower = load "simple_load/lower.groovy"
+println "Should be 'Good morning Polly' and 'Hello Tommy'"
 
+lower = load "simple_load/lower.groovy"
+lower.configure_greeting("Good morning")
+lower.say_hi("Polly")
+
+
+lower = load "simple_load/lower.groovy"
 lower.say_hi("Tommy")
+
